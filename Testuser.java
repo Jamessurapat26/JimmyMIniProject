1873f761e12375cc790a25b4da267f149abee6be
Jamessurapat26 <james262600@gmail.com>
2023-12-12 22:52:04 +0700

    edit

--- Testuser.java
+++ Testuser.java
@@ -3,9 +3,6 @@ import java.util.*;
 public class Testuser {
     public static void main(String[] args) {
 
-    Date expirationDate = new Date();
-
-
     ArrayList<Student> member = new ArrayList<Student>();
     member.add(new Student("00000","11111","Jimmy"));
     member.add(new Student("00000","11111","Jimmy"));
@@ -15,4 +12,4 @@ public class Testuser {
     }
 
     }
-    }
+}
